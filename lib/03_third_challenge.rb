a141caf4b658b2130af2c244213a73c0ba9aac7a
Marcia Ann Freeman <marciafreeman2011@gmail.com>
2020-10-11 02:39:25 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,20 +2,24 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name "Lord Montague",
-        
+        :name => "Lord Montague",
+        :age => 53
         },
       :matriarch => {
-        #your key/value pairs here
+        :name =>
+        :age =>
         },
       :hero => {
-        #your key/value pairs here
+        :name =>
+        :age =>
+        :status =>
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+                :name =>
+        :age =>
         },
       :matriarch => {
         #your key/value pairs here
